97397af29a270f9d90f09f1379bd10610b43130a
Nicholas Zolton <nicholaszolton+git@gmail.com>
2023-09-16 13:50:19 -0500

    Added basic backend

---
+++ backend/main.py
@@ -0,0 +1,9 @@
+from sklearn.ensemble import GradientBoostingRegressor
+
+if __name__ == "__main__":
+    LOWER_ALPHA = 0.05
+    HIGHER_ALPHA = 0.95
+
+    lower_model = GradientBoostingRegressor(loss="quantile", alpha=LOWER_ALPHA)
+    middle_model = GradientBoostingRegressor(loss="ls")
+    higher_model = GradientBoostingRegressor(loss="quantile", alpha=HIGHER_ALPHA)
